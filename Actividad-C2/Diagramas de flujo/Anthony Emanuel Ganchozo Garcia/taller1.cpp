c10b5d76d278b033c5e2f43579ec44c4a7009251
AnthonyGanchozo <92495993+AnthonyGanchozo@users.noreply.github.com>
2021-12-17 11:06:24 -0500

    Add files via upload

---
+++ Actividad-C2/Diagramas de flujo/Anthony Emanuel Ganchozo Garcia/taller1.cpp
@@ -0,0 +1,15 @@
+/*Haciendo la suma de dos numeros utilizando c++*/
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,y,z;
+	cout<<"Ingrese el primer valor: ";cin>>x;
+	cout<<"Ingrese el segundo  valor: ";cin>>y;
+	z=x+y;
+	cout<<"La suma de dichos numeros ingresados es: "<<z<<endl;
+	
+	return 0;
+
+
+}
